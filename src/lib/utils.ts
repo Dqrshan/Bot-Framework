ea34446390055f41883ff54a8fdcb171b573986e
Nin <darshan310268@gmail.com>
2023-04-11 11:26:00 +0530

    feat: permission checks

---
+++ src/lib/utils.ts
@@ -0,0 +1,21 @@
+import { GuildMember, PermissionResolvable, TextChannel } from 'discord.js';
+
+export const doPermissionCheck = (
+	channel: TextChannel,
+	member: GuildMember,
+	permission: PermissionResolvable
+): boolean => {
+	if (channel.permissionsFor(member!).has(permission)) return true;
+	if (member!.permissions.has(permission)) return true;
+	return false;
+};
+
+export const doClientPermissionCheck = (
+	channel: TextChannel,
+	member: GuildMember,
+	permission: PermissionResolvable
+) => {
+	if (channel.permissionsFor(member!).has(permission)) return true;
+	if (member!.permissions.has(permission)) return true;
+	return false;
+};
